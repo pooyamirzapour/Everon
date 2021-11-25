412f3a5b6be347125e3f1fe0d665dea5dbd7ae55
Pooyamirzapour <pooyamirzapour@yahoo.com>
2021-11-25 21:18:02 +0330

    init

--- src/test/java/com/everon/assignment/AbstractTest.java
+++ src/test/java/com/everon/assignment/AbstractTest.java
@@ -5,12 +5,18 @@ import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.SpyBean;
+import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ExtendWith(SpringExtension.class)
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class AbstractTest {
-    @Autowired
+
+    @SpyBean
     public ChargingSessionRepository chargingSessionRepository;
+
+    @Autowired
+    public TestRestTemplate rest;
 }
---
+++ src/test/java/com/everon/assignment/api/ChargingSessionControllerTest.java
@@ -0,0 +1,77 @@
+package com.everon.assignment.api;
+
+import com.everon.assignment.AbstractTest;
+import com.everon.assignment.model.dto.NewSessionRequest;
+import com.everon.assignment.model.entity.CarChargingSession;
+import com.everon.assignment.model.entity.Summary;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.*;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+class ChargingSessionControllerTest extends AbstractTest {
+
+    @LocalServerPort
+    private int localPort;
+
+    @Test
+    void should_returnSuccess_when_inNewSession_stationIdIsSet() {
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Content-Type", MediaType.APPLICATION_JSON_VALUE);
+        NewSessionRequest request = new NewSessionRequest("ABC-123456");
+        HttpEntity<?> entity = new HttpEntity<>(request, headers);
+        String uri = String.format("http://localhost:%s/chargingSessions", localPort);
+        ResponseEntity<CarChargingSession> response = rest.exchange(uri, HttpMethod.POST, entity, CarChargingSession.class);
+        Assertions.assertEquals(HttpStatus.OK, response.getStatusCode());
+        chargingSessionRepository.clear();
+    }
+
+    @Test
+    void should_returnSuccess_when_inStoppedSession_IdIsSet() {
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Content-Type", MediaType.APPLICATION_JSON_VALUE);
+
+        NewSessionRequest request = new NewSessionRequest("ABC-123456");
+        HttpEntity<?> entity = new HttpEntity<>(request, headers);
+        String uri = String.format("http://localhost:%s/chargingSessions", localPort);
+        ResponseEntity<CarChargingSession> exchange = rest.exchange(uri, HttpMethod.POST, entity, CarChargingSession.class);
+        entity = new HttpEntity<>(null, headers);
+        uri = String.format("http://localhost:%s/chargingSessions/"+exchange.getBody().getId() , localPort);
+        ResponseEntity<CarChargingSession> response = rest.exchange(uri, HttpMethod.PUT, entity, CarChargingSession.class);
+        Assertions.assertEquals(HttpStatus.OK, response.getStatusCode());
+        chargingSessionRepository.clear();
+
+    }
+
+    @Test
+    void should_returnSuccess_when_getAllSessionIsCalled() {
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Content-Type", MediaType.APPLICATION_JSON_VALUE);
+        HttpEntity<?> entity = new HttpEntity<>(null, headers);
+        String uri = String.format("http://localhost:%s/chargingSessions", localPort);
+        ResponseEntity<CarChargingSession[]> response = rest.exchange(uri, HttpMethod.GET, entity, CarChargingSession[].class);
+        Assertions.assertEquals(HttpStatus.OK, response.getStatusCode());
+    }
+
+    @Test
+    void should_returnSuccess_when_SummaryReportIsCalled() {
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Content-Type", MediaType.APPLICATION_JSON_VALUE);
+
+        NewSessionRequest request = new NewSessionRequest("ABC-123456");
+        HttpEntity<?> entity = new HttpEntity<>(request, headers);
+        String uri = String.format("http://localhost:%s/chargingSessions", localPort);
+         rest.exchange(uri, HttpMethod.POST, entity, CarChargingSession.class);
+
+        entity = new HttpEntity<>(null, headers);
+         uri = String.format("http://localhost:%s/chargingSessions/summary", localPort);
+        ResponseEntity<Summary> response = rest.exchange(uri, HttpMethod.GET, entity, Summary.class);
+        Assertions.assertEquals(HttpStatus.OK, response.getStatusCode());
+        Assertions.assertEquals(response.getBody().getStartedCount(),1);
+    }
+}
