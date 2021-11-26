b829d7a27e1169645e7167bfe96849b3875f064b
Pooyamirzapour <pooyamirzapour@yahoo.com>
2021-11-26 14:44:25 +0330

    readme

--- src/main/java/com/everon/assignment/service/ChargingSessionServiceImpl.java
+++ src/main/java/com/everon/assignment/service/ChargingSessionServiceImpl.java
@@ -21,21 +21,40 @@ public class ChargingSessionServiceImpl implements ChargingSessionService {
 
     private ChargingSessionRepository chargingSessionRepository;
 
+    /**
+     * New Session
+     * @param stationId
+     * @return CarChargingSession
+     */
     @Override
     public CarChargingSession newSession(String stationId) {
         return chargingSessionRepository.save(stationId.trim());
     }
 
+    /**
+     * Stop session
+     * @param stationId
+     * @return CarChargingSession
+     * @throws ApiServiceException
+     */
     @Override
     public CarChargingSession stopSession(String stationId) throws ApiServiceException {
         return chargingSessionRepository.update(stationId);
     }
 
+    /**
+     * Get all sessions
+     * @return List<CarChargingSession>
+     */
     @Override
     public List<CarChargingSession> getSessions() {
         return chargingSessionRepository.findAll();
     }
 
+    /**
+     * Get summary in a last minute
+     * @return Summary
+     */
     @Override
     public Summary summary() {
         return chargingSessionRepository.summary();
