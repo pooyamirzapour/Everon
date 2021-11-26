7b4361be03c4d5f279afb17c28614c544c2c82fc
Pooyamirzapour <pooyamirzapour@yahoo.com>
2021-11-26 15:16:49 +0330

    readme

--- src/main/java/com/everon/assignment/model/dto/NewSessionRequest.java
+++ src/main/java/com/everon/assignment/model/dto/NewSessionRequest.java
@@ -4,15 +4,13 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
-
 /**
  * NewSessionRequest DTO class for new Session.
  * @author Pooya Mirzapour (pooyamirzapour@gmail.com)
  */
-
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class NewSessionRequest {
     private String stationId;
 }
