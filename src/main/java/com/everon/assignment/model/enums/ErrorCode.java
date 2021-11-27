5de4229e93b76fea5f95b3669c413d4fa2010c79
Pooyamirzapour <pooyamirzapour@yahoo.com>
2021-11-27 09:21:57 +0330

    error code

--- src/main/java/com/everon/assignment/model/enums/ErrorCode.java
+++ src/main/java/com/everon/assignment/model/enums/ErrorCode.java
@@ -7,7 +7,7 @@ package com.everon.assignment.model.enums;
 public enum ErrorCode {
 
    ID_IS_NOT_VALID("Id is not valid",1001),
-   ID_IS_ALREADY_STOPPED("Id is already stopped",1001);
+   ID_IS_ALREADY_STOPPED("Id is already stopped",1002);
 
     private int value;
     private String message;
